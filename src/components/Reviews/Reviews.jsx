513b1267ec5c87141635a2e135867fb33b29bd0c
Artem Hordiyevskyy <139376262+slonic32@users.noreply.github.com>
2025-09-08 23:44:40 +0300

    add review

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,9 +1,89 @@
 import { useSelector } from "react-redux";
 import css from "./Reviews.module.css";
 import { selectCamper } from "../../redux/campers/selectors";
+import icon from "../../assets/icon.svg";
 
 export default function Reviews() {
   const camper = useSelector(selectCamper);
   if (!camper || Object.keys(camper).length === 0) return null;
-  return <p>review</p>;
+  return (
+    <div className={css.block}>
+      <ul className={css.list}>
+        {camper.reviews.map(rev => (
+          <li>
+            <div className={css.reviewBox}>
+              <div className={css.avatar}>
+                <span className={css.letter}>{rev.reviewer_name[0]}</span>
+              </div>
+              <div className={css.reviewRight}>
+                <p className={css.name}>{rev.reviewer_name}</p>
+                <div>
+                  <ul className={css.rate}>
+                    <li>
+                      {rev.reviewer_rating >= 0.5 ? (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-yellow`}></use>
+                        </svg>
+                      ) : (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-gray`}></use>
+                        </svg>
+                      )}
+                    </li>
+                    <li>
+                      {rev.reviewer_rating >= 1.5 ? (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-yellow`}></use>
+                        </svg>
+                      ) : (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-gray`}></use>
+                        </svg>
+                      )}
+                    </li>
+                    <li>
+                      {rev.reviewer_rating >= 2.5 ? (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-yellow`}></use>
+                        </svg>
+                      ) : (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-gray`}></use>
+                        </svg>
+                      )}
+                    </li>
+                    <li>
+                      {rev.reviewer_rating >= 3.5 ? (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-yellow`}></use>
+                        </svg>
+                      ) : (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-gray`}></use>
+                        </svg>
+                      )}
+                    </li>
+                    <li>
+                      {rev.reviewer_rating >= 4.5 ? (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-yellow`}></use>
+                        </svg>
+                      ) : (
+                        <svg width="16" height="16" className={css.star}>
+                          <use href={`${icon}#star-gray`}></use>
+                        </svg>
+                      )}
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <div>
+              <p className={css.description}>{rev.comment}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
